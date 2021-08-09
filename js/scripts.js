66e1c2aa191dd2dc46a604cce740ff3e0d17bf23
Ahmad <ahmad.anadani1994@gmail.com>
2021-08-09 19:58:40 +0200

    with arabic content

--- js/scripts.js
+++ js/scripts.js
@@ -52,8 +52,8 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
+    //new SimpleLightbox({
+      //  elements: '#portfolio a.portfolio-box'
+    //});
 
 });
